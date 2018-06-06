c070f687a3bf6abae7a2a444cebcd89dc2af9a6f
kongxiangshuai <kxsh2015@163.com>
2018-06-07 00:54:17 +0800

    update application.properties

--- hello/src/main/java/com/tuyoo/hello/HelloApplication.java
+++ hello/src/main/java/com/tuyoo/hello/HelloApplication.java
@@ -1,14 +1,11 @@
 package com.tuyoo.hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,11 +16,7 @@ public class HelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
-//    @Value("${server.port}")
-//    String port;
-//
-//    @Value("${from}")
-//    String from;
+
     @Autowired
     private Environment environment;
 
