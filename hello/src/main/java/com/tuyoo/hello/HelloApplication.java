957c58d94665cdbde87df1a023f1f7022f36fbda
kongxiangshuai <kxsh2015@163.com>
2018-06-07 14:44:32 +0800

    update application.properties

--- hello/src/main/java/com/tuyoo/hello/HelloApplication.java
+++ hello/src/main/java/com/tuyoo/hello/HelloApplication.java
@@ -3,13 +3,19 @@ package com.tuyoo.hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class HelloApplication {
 
@@ -19,6 +25,11 @@ public class HelloApplication {
 
     @Autowired
     private Environment environment;
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     @RequestMapping("/hi")
     public String home() {
---
+++ hello/src/main/java/com/tuyoo/hello/controller/DcController.java
@@ -0,0 +1,27 @@
+package com.tuyoo.hello.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class DcController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("hello");
+        String url = String.format("http://%s:%s/hi", serviceInstance.getHost(), serviceInstance.getPort());
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
