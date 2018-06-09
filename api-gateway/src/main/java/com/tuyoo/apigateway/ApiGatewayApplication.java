a4fad6967f7a264c0b268bec2d35a10c01071f9f
kongxiangshuai <kxsh2015@163.com>
2018-06-09 22:55:03 +0800

    update application.properties

--- api-gateway/src/main/java/com/tuyoo/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/tuyoo/apigateway/ApiGatewayApplication.java
@@ -4,11 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-public class ApiGatewayApplication {
+@Configuration
+public class ApiGatewayApplication extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
--- hello/src/main/java/com/tuyoo/hello/HelloApplication.java
+++ hello/src/main/java/com/tuyoo/hello/HelloApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
